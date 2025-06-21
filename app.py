c93df4b71ffa4cdac44c917407bf0cfcaee8ec46
Ubuntu <ubuntu@ip-172-31-29-25.ec2.internal>
2025-06-21 09:12:27 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+tasks = [
+    {"id": 1, "task": "Buy groceries", "done": False},
+    {"id": 2, "task": "Read a book", "done": False}
+]
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({"tasks": tasks})
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    data = request.json
+    task = {
+        "id": len(tasks) + 1,
+        "task": data['task'],
+        "done": False
+    }
+    tasks.append(task)
+    return jsonify(task), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
